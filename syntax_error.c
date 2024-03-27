8c6d7fe51359af5dc32bf3e565d14cff4083391a
Salah Demnati <sdemnati@e3r2p3.1337.ma>
2024-03-27 18:39:06 +0000

    fix syntax errs

--- syntax_error.c
+++ syntax_error.c
@@ -5,41 +5,46 @@ int	ft_type(t_list *lst)
 	if (!lst)
 		return (0);
 	if (lst->type == R_OUT || lst->type == R_IN || lst->type == HER_DOC 
-		|| lst->type == APPEND || lst->type == PIPE)
+		|| lst->type == APPEND)
 		return (2);
 	if (lst->type == WORD || lst->type == D_Q
 		|| lst->type == S_Q || lst->type == VARIABLE)
 		return (3);
+	if (lst->type == PIPE)
+		return  (4);
 	return (0);
 }
 
-void	ft_syntax_error(t_list **list)
+int	ft_syntax_error(t_list **list)
 {
 	t_list	*tmp;
 
 	tmp = *list;
 	if (!tmp)
-		return ;
-	if (tmp->type == PIPE || ft_type(ft_lstlast(tmp)) == 2)
-		return (ft_putstr_fd("11\n", 2));
+		return 1;
+	if (tmp->type == PIPE || ft_type(ft_lstlast(tmp)) == 2 || ft_lstlast(tmp)->type == PIPE)
+		return (write(2, "11\n", 3), 1);
 	while (tmp)
 	{
-		if (ft_type(tmp) == 2 && ft_type(tmp->next) != 3
-			&& ft_type(tmp->next->next) != 3)
-				return (ft_putstr_fd("22\n", 2));
+		if (ft_type(tmp) == 2 && ft_type(tmp->next) != 3 && ft_type(tmp->next->next) != 3)
+			return (write(2, "11\n", 3), 1);
+		if (ft_type(tmp) == 4 && (ft_type(tmp->next) == 4 || ft_type(tmp->next->next) == 4))
+			return (write(2, "11\n", 3), 1);
 		tmp->skip = false;
 		tmp = tmp->next;
 	}
-	tmp = *list;
-	while (tmp)
-	{
-		if (tmp->type == D_Q || tmp->type == VARIABLE)
-			ft_expand(tmp);
-		else if(tmp->type == SPACE_)
-			printf(" ");
-		else
-			printf("%s", tmp->value);
-		tmp = tmp->next;	
-	}
-	printf("\n");
+	return 0;
+		// return (ft_putstr_fd("11\n", 2));
+	// tmp = *list;
+	// while (tmp)
+	// {
+	// 	if (tmp->type == D_Q || tmp->type == VARIABLE)
+	// 		ft_expand(tmp);
+	// 	else if(tmp->type == SPACE_)
+	// 		printf(" ");
+	// 	else
+	// 		printf("%s", tmp->value);
+	// 	tmp = tmp->next;	
+	// }
+	// printf("\n");
 }
