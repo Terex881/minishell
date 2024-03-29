89ed0d73d926b55e05be262d67c935813d881b12
Chorouk Masnaoui <cmasnaou@e3r4p16.1337.ma>
2024-03-29 22:14:39 +0000

    added exit and fix pwd and cd

--- builtins.c
+++ builtins.c
@@ -12,7 +12,7 @@ static int	check_n(char *s)
 	}
 	return (1);	
 }
-void ft_echo(char **arg)
+void	ft_echo(char **arg)
 {
     int i = 0;
     int n = 0;
@@ -36,27 +36,81 @@ void ft_echo(char **arg)
         write(1, "\n", 1);
 }
 
-void ft_pwd(char *arg)
-{
-	char tab[100];
+// void	ft_pwd(char *arg)
+// {
+// 	char tab[100];
 
-	getcwd(tab, 100);
-	write(1, tab, ft_strlen(tab));
+// 	getcwd(tab, 100);
+// 	write(1, tab, ft_strlen(tab));
+// 	write(1, "\n", 1);
+// }
+
+// static char	*old_pwd(char **env)//🌸
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (env[i])
+// 	{
+// 		if (ft_strncmp(env[i], "OLDPWD", 6) == 0)//pwd
+// 			return (env[i] + 7);
+// 		i++;
+// 	}
+// 	return (NULL);
+// }
+
+
+
+void ft_pwd(t_env *env, t_data *data)//update pwd in env!!🌸
+{
+	// char tab[100];
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+	{
+		if (errno == ENOENT)
+			// printf("path no longer exist \n");
+			printf("%s\n", ft_lstfind_env(&env, "PWD", NULL)->line + 4);//pwd or oldpwd? //add /..
+		// write(1, "Error\n", 6);
+		return ;
+	}
+	write(1, pwd, ft_strlen(pwd));
 	write(1, "\n", 1);
 }
 
-void ft_env(char **env)
+void	ft_env(t_var *exec, t_data *data)
 {
-	while (*env)
+	t_data *q;
+	t_env *p;
+	t_env *tmp;
+	
+	q = data;
+	p = data->env;
+	// exec->old_pwd = old_pwd(exec->env);
+	if (getcwd(NULL, 0) != NULL)
 	{
-		write(1, *env, ft_strlen(*env));
-		write(1, "\n", 1);
-		env++;
+		// while (q)
+		// {
+			tmp = ft_lstfind_env(&q->env, "PWD", ft_strjoin("PWD=", getcwd(NULL, 0)));
+			q->old_pwd = ft_strjoin("PWD=", getcwd(NULL, 0));
+			// printf(" ==> %s\n\n", q->old_pwd);
+		// 	q = q->next;
+		// }
 	}
+	// printf("---- %s\n\n", tmp->line);
+	// else
+		// printf(" ==> %s\n\n", q->old_pwd);
+    while (p)
+    {
+        printf("%s\n", p->line);
+        p = p->next;
+    }
 }
 
-void ft_cd(char *path)
+void	ft_cd(char *path, t_data *data)
 {
+	(void)data;
 	if (!path || (ft_strlen(path) == 1 && path[0] == '~'))
 	{
 		if (chdir(getenv("HOME")) < 0)
@@ -64,5 +118,11 @@ void ft_cd(char *path)
 	}
 	else if (chdir(path) < 0)
 		return (perror(NULL));
+}
+
+void	ft_exit()//🌸
+{
+	write(1, "exit\n", 5);
+	exit(0);
 }
 
