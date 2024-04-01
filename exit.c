8593fb38e3f8a839fac5803425139f6094ecef3f
Chorouk Masnaoui <cmasnaou@e3r1p1.1337.ma>
2024-04-01 12:02:17 +0000

    exit

---
+++ exit.c
@@ -0,0 +1,57 @@
+#include "minishell.h"
+
+static long long	ft_valid_arg(char *str, int *valid)
+{
+	short               s;
+	unsigned long long  n;
+	unsigned long long  tmp;
+
+	s = 1;
+	n = 0;
+    *valid = 0;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			s *= -1;
+		(str)++;
+	}
+	while (*str)
+	{
+        if (!(*str >= '0' && *str <= '9'))
+            return (0); 
+		tmp = n;
+		n = n * 10 + *str - '0';
+        if (tmp > n || n > 1844674407370955169)
+            return (0);
+		str++;
+	}
+    *valid = 1;
+	return (s * n);
+}
+
+void	ft_exit(t_var *exec, t_data **data, char **arg)
+{
+    int valid = 1;
+    long long n;
+	(void)data;
+
+	// clear data before exit
+	// ft_lstclear_env(&(*data)->env);
+	// free((*data)->path);
+	// free(*data);
+    // printf("-- > %s    %d   %lld\n", arg, valid, n);
+	write(exec->f_out, "exit\n", 5);
+    if (!arg || !arg[1])
+        exit(0);
+    if (arg[2])
+        return ((void)write(2, "minishell: exit: too many arguments\n", 36));
+    n = ft_valid_arg(arg[1], &valid);
+    if (!valid)
+    {
+        write(2, "minishell: exit: ", 17);
+        write(2, arg[1], ft_strlen(arg[1]));
+        write(2, ": numeric argument required\n", 29);
+        exit(255);
+    }
+	exit(n);
+}
