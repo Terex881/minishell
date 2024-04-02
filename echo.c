658c6cdae676ef440b8bddd5a09227e951a9235a
Chorouk Masnaoui <cmasnaou@e3r1p2.1337.ma>
2024-04-02 17:12:01 +0000

    norminette fixed

--- echo.c
+++ echo.c
@@ -2,8 +2,9 @@
 
 static int	check_n(char *s)
 {
-	int i = 0;
+	int	i;
 
+	i = 0;
 	if (s[0] != '-')
 		return (0);
 	if (!s[1])
@@ -13,32 +14,34 @@ static int	check_n(char *s)
 		if (s[i] != 'n')
 			return (0);
 	}
-	return (1);	
+	return (1);
 }
 
 void	ft_echo(char **arg, t_var *exec)
 {
-    int i = 0;
-    int n = 0;
+	int	i;
+	int	n;
 
+	i = 0;
+	n = 0;
 	if (!arg || !*arg)
 		return ((void)write(exec->f_out, "\n", 1));
 	if (check_n(arg[0]))
-        (n = 1, i++);
+		(1) && (n = 1, i++);
 	if (!arg[i])
 		return ;
-    while (arg && arg[i])
-    {
+	while (arg && arg[i])
+	{
 		if (n == 1 && check_n(arg[i]))
-        {
+		{
 			i++;
 			continue ;
 		}
 		ft_putstr_fd(arg[i], exec->f_out);
-        if (arg[i] && arg[i + 1])
-        	write(exec->f_out, " ", 1);
-        i++;
-    }
-    if (!n)
-        write(exec->f_out, "\n", 1);
+		if (arg[i] && arg[i + 1])
+			write(exec->f_out, " ", 1);
+		i++;
+	}
+	if (!n)
+		write(exec->f_out, "\n", 1);
 }
