d562f05caade6015ef449121ca99f864c828b7cc
Chorouk Masnaoui <cmasnaou@e3r1p2.1337.ma>
2024-04-02 16:46:06 +0000

    .

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ void ft_signal_c(int num)
 {
     if (num == SIGINT)
 	{
-		rl_clear_history();
+		// rl_clear_history();
 		write(1, "\n", 1);
 		rl_on_new_line();
-		rl_replace_line("", 0);
+		// rl_replace_line("", 0);
 		rl_redisplay();
     }
 }
@@ -19,15 +19,17 @@ void ft_signal()
     signal(SIGQUIT, ft_signal_c);
 }
 
+void leak(){system("leaks minishell");}
 
 int	main(int ac, char **av, char **env)
 {
+	atexit(leak);
 	(void)ac;
 	(void)av;
 	t_list	*list;
 	t_data	*data;
 
-	rl_catch_signals = 0;
+	// rl_catch_signals = 0;
 	list = NULL;
 	ft_get_env(&data, env);
 	
