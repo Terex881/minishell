65004922143f12979551184d7e6256d8273d80c4
Chorouk Masnaoui <cmasnaou@e3r1p2.1337.ma>
2024-04-04 21:52:59 +0000

    .

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -4,10 +4,15 @@ void	ft_pwd(t_var *exec, t_env *env)
 {
 	char	*tmp;
 
-	tmp = ft_lstfind_env(&env, "PWD", NULL);
-	if(!tmp)
-		return;
-	ft_putstr_fd(tmp + 4, exec->f_out);
+	// tmp = ft_lstfind_env(&env, "PWD", NULL);
+	tmp = getcwd(NULL, 0);
+	// if (!tmp)
+	// {
+	// 	ft_error("minishell: pwd: ", NULL, strerror(errno));
+	// 	exec->status = 1;
+	// 	return ;
+	// }
+	ft_putstr_fd(tmp, exec->f_out);
 	ft_putstr_fd("\n", exec->f_out);
 	free(tmp);
 	return ;
