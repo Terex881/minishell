0036a7324da0a9832efe43f6ba046ee57c24046a
Chorouk Masnaoui <cmasnaou@e3r1p2.1337.ma>
2024-04-04 21:53:14 +0000

    .

--- built_in/unset.c
+++ built_in/unset.c
@@ -1,16 +1,18 @@
 #include "../minishell.h"
 
-void	ft_unset(t_var *exec, t_data *data, char *line)
+void	ft_unset(t_var *exec, t_data **data, char *line)
 {
 	t_env	*p;
 	t_env	*tmp;
 
-	p = data->env;
+	if (!data || !(*data) || !(*data)->env || !line)
+		return ;
+	p = (*data)->env;
 	tmp = NULL;
 	if (ft_strncmp(line, "PATH", 5) == 0)
 	{
-		free(data->path);
-		data->path = NULL;
+		free((*data)->path);
+		(*data)->path = NULL;
 	}
 	while (p)
 	{
@@ -19,12 +21,43 @@ void	ft_unset(t_var *exec, t_data *data, char *line)
 			if (tmp)
 				tmp->next = p->next;
 			else
-				data->env = p->next;
+				(*data)->env = p->next;
 			free(p->line);
-			free(p);
+			// free(p);
 			return ;
 		}
 		tmp = p;
 		p = p->next;
 	}
 }
+
+// void	ft_unset(t_var *exec, t_data **data, char *line)
+// {
+// 	t_env *p;
+
+// 	if (!data || !(*data) || !(*data)->env || !line)
+// 		return ;
+// 	if (ft_strncmp(line, "PATH", 5) == 0)
+// 	{
+// 		free((*data)->path);
+// 		(*data)->path = NULL;
+// 	}
+// 	if (ft_strncmp((*data)->env->line, line, ft_strlen(line)) == 0)
+// 	{
+// 		free((*data)->env->line);
+// 		(*data)->env = (*data)->env->next;
+// 		return ;
+// 	}
+// 	p = (*data)->env;
+// 	while (p->next)
+// 	{
+// 		if (ft_strncmp(p->next->line, line, ft_strlen(line)) == 0)
+// 		{
+// 			free(p->next->line);
+// 			free(p->next);
+// 			p->next = p->next->next;
+// 			return ;
+// 		}
+// 		p = p->next;
+// 	}
+// }
