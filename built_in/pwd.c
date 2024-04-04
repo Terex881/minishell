bbc8b3dcac5744f4973d8c4901da83172a4d5679
Chorouk Masnaoui <cmasnaou@e3r1p2.1337.ma>
2024-04-04 17:11:59 +0000

    .

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -2,11 +2,12 @@
 
 void	ft_pwd(t_var *exec, t_env *env)
 {
-	t_env	*tmp;
+	char	*tmp;
 
 	tmp = ft_lstfind_env(&env, "PWD", NULL);
-	ft_putstr_fd(tmp->line + 4, exec->f_out);
+	ft_putstr_fd(tmp + 4, exec->f_out);
 	ft_putstr_fd("\n", exec->f_out);
 	// return ;
+	free(tmp);
 	exit(89); // 0
 }
