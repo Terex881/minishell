cd120a91d5ea189e87a446612321ef1c1c65606c
Cho-r-ouk <149368627+Cho-r-ouk@users.noreply.github.com>
2024-04-06 23:05:40 +0000

    norminette fixed

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -1,17 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmasnaou <cmasnaou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/06 20:20:39 by cmasnaou          #+#    #+#             */
+/*   Updated: 2024/04/06 20:20:40 by cmasnaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
 void	ft_pwd(t_var *exec, t_env *env)
 {
 	char	*tmp;
 
-	// tmp = ft_lstfind_env(&env, "PWD", NULL);
 	tmp = getcwd(NULL, 0);
-	// if (!tmp)
-	// {
-	// 	ft_error("minishell: pwd: ", NULL, strerror(errno));
-	// 	exec->status = 1;
-	// 	return ;
-	// }
 	ft_putstr_fd(tmp, exec->f_out);
 	ft_putstr_fd("\n", exec->f_out);
 	free(tmp);
