d81f29d225e4f7f9bfe4ae13ccb15a376228fdcb
Chorouk Masnaoui <cmasnaou@e3r1p17.1337.ma>
2024-04-06 23:51:47 +0000

    pwd fixed/norminette fixed

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -6,7 +6,7 @@
 /*   By: cmasnaou <cmasnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 20:20:39 by cmasnaou          #+#    #+#             */
-/*   Updated: 2024/04/06 20:20:40 by cmasnaou         ###   ########.fr       */
+/*   Updated: 2024/04/06 23:50:58 by cmasnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,18 @@ void	ft_pwd(t_var *exec, t_env *env)
 {
 	char	*tmp;
 
-	tmp = getcwd(NULL, 0);
-	ft_putstr_fd(tmp, exec->f_out);
-	ft_putstr_fd("\n", exec->f_out);
-	free(tmp);
-	return ;
+	tmp = ft_lstfind_env(&env, "PWD", NULL);
+	if (tmp)
+	{
+		ft_putstr_fd(tmp + 4, exec->f_out);
+		ft_putstr_fd("\n", exec->f_out);
+	}
+	else
+	{
+		tmp = getcwd(NULL, 0);
+		ft_putstr_fd(tmp, exec->f_out);
+		ft_putstr_fd("\n", exec->f_out);
+		free(tmp);
+		return ;
+	}
 }
