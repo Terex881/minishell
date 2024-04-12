bceb75be89d7f591e9dc6444ae356b27d9ddbb2e
Chorouk Masnaoui <cmasnaou@e3r10p18.1337.ma>
2024-04-12 09:15:50 +0000

    norm fixed

--- expand.c
+++ expand.c
@@ -6,7 +6,7 @@
 /*   By: cmasnaou <cmasnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 23:09:35 by sdemnati          #+#    #+#             */
-/*   Updated: 2024/04/11 13:08:22 by cmasnaou         ###   ########.fr       */
+/*   Updated: 2024/04/12 08:57:42 by cmasnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ void	ft_expand(t_list **list, t_data *data)
 		if (tmp->type == HER_DOC)
 			tmp = ft_next(tmp->next);
 		else if (tmp && !ft_strcmp(tmp->value, "$?"))
-			tmp->value = ft_itoa(data_stat);
+			tmp->value = ft_itoa(g_stat);
 		else if (tmp && (tmp->type == D_Q || tmp->type == VARIABLE))
 			tmp->value = ft_search_var(tmp->value, data);
 		else if (tmp && tmp->next && !ft_strcmp(tmp->value, "$")
