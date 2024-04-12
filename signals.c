4b82f8448dc5d22835e0aca059f5c70a6629e083
Chorouk Masnaoui <cmasnaou@e3r10p18.1337.ma>
2024-04-12 09:16:24 +0000

    norm fixed

--- signals.c
+++ signals.c
@@ -6,14 +6,15 @@
 /*   By: cmasnaou <cmasnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 00:47:37 by sdemnati          #+#    #+#             */
-/*   Updated: 2024/04/11 16:32:45 by cmasnaou         ###   ########.fr       */
+/*   Updated: 2024/04/12 08:59:26 by cmasnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_signal_her()
+void	ft_signal_her(int num)
 {
+	(void)num;
 	close(0);
 }
 
@@ -21,7 +22,7 @@ void	ft_signal_c(int num)
 {
 	if (num == SIGINT)
 	{
-		data_stat = 1;
+		g_stat = 1;
 		if (wait(NULL) != -1)
 			return (ft_putstr_fd("\n", 1));
 		rl_clear_history();
