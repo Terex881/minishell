b665f7ccaa3cbaf8d78b6868f4b5d19c2add2c7f
Chorouk Masnaoui <cmasnaou@e3r3p17.1337.ma>
2024-04-26 21:48:32 +0100

    pwd fixed

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -6,13 +6,13 @@
 /*   By: cmasnaou <cmasnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 20:20:39 by cmasnaou          #+#    #+#             */
-/*   Updated: 2024/04/06 23:50:58 by cmasnaou         ###   ########.fr       */
+/*   Updated: 2024/04/26 18:06:59 by cmasnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_pwd(t_var *exec, t_env *env)
+void	ft_pwd(t_var *exec, t_env *env, t_data *data)
 {
 	char	*tmp;
 
@@ -25,6 +25,12 @@ void	ft_pwd(t_var *exec, t_env *env)
 	else
 	{
 		tmp = getcwd(NULL, 0);
+		if (!tmp)
+		{
+			ft_putstr_fd(data->pwd, exec->f_out);
+			ft_putstr_fd("\n", exec->f_out);
+			return ;
+		}
 		ft_putstr_fd(tmp, exec->f_out);
 		ft_putstr_fd("\n", exec->f_out);
 		free(tmp);
