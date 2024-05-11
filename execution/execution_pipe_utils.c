ca9e513b4bf436767c19e10053535b5d326301b3
Chorouk Masnaoui <cmasnaou@e3r4p15.1337.ma>
2024-05-11 18:13:51 +0100

    norm

--- execution/execution_pipe_utils.c
+++ execution/execution_pipe_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution_pipe_utils.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sdemnati <sdemnati@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cmasnaou <cmasnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:53:14 by cmasnaou          #+#    #+#             */
-/*   Updated: 2024/05/11 10:43:07 by sdemnati         ###   ########.fr       */
+/*   Updated: 2024/05/11 18:11:05 by cmasnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,33 +28,45 @@ void	ft_close_pipe(t_data *data)
 	close(data->pipe_ends[1]);
 }
 
+char	*valid_cmd(char *cmd, char **paths, char *path)
+{
+	char	*tmp;
+	int		i;
+
+	i = -1;
+	if (!paths)
+		return (exit_status(127, 1), NULL);
+	if ((cmd[0] == '.' || cmd[0] == '/') && !access(cmd, F_OK))
+		(ft_error(cmd, ": Permission denied"), exit(exit_status(126, 1)));
+	if (cmd[0] == '.' || cmd[0] == '/')
+		return (exit_status(127, 1), NULL);
+	while (paths && paths[++i])
+	{
+		(1) && (tmp = ft_strjoin(paths[i], "/"), path = ft_strjoin(tmp, cmd));
+		if (!access(path, F_OK | X_OK))
+			return (path);
+	}
+	if (ft_strchr(cmd, '/'))
+	{
+		ft_error(cmd, ": No such file or directory");
+		exit(exit_status(127, 1));
+	}
+	(ft_error(cmd, ": command not found"), exit(exit_status(127, 1)));
+}
+
 char	*valid_path(char *cmd, char *line)
 {
-	int		i;
 	char	*path;
-	char	*tmp;
-	char	**paths;
 
+	path = NULL;
+	if (!ft_strcmp(cmd, "minishell"))
+		return (ft_error(cmd, ": command not found"), \
+		exit(exit_status(127, 1)), NULL);
 	if (cmd && ft_strlen(cmd))
 	{
-		(check_cmd(cmd), i = -1);
 		if (!access(cmd, F_OK | X_OK))
 			return (ft_strdup(cmd));
-		paths = get_paths(line);
-		if (!paths)
-			return (exit_status(127, 1), NULL);
-		if ((cmd[0] == '.' || cmd[0] == '/') && !access(cmd, F_OK))
-			(ft_error(cmd, ": Permission denied"), exit(exit_status(126, 1)));
-		if (cmd[0] == '.' || cmd[0] == '/')
-			return (exit_status(127, 1), NULL);
-		while (paths && paths[++i])
-		{
-			(1) && (tmp = ft_strjoin(paths[i], "/"), path = ft_strjoin(tmp, cmd));
-			if (!access(path, F_OK | X_OK))
-				return (path);
-		}
-		if (ft_strchr(cmd, '/'))
-			(ft_error(cmd, ": No such file or directory"), exit(exit_status(127, 1)));
+		return (check_cmd(cmd), valid_cmd(cmd, get_paths(line), path));
 	}
 	(ft_error(cmd, ": command not found"), exit(exit_status(127, 1)));
 }
